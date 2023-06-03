5458d7cbac917190f2b3a8e9ed79f8451f1c581c
fauzandp <fauzan27dp@gmail.com>
2023-06-03 22:03:58 +0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Fauzan Dwi Prasetyo on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
